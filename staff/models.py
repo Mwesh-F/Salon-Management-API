bbacc31f368d33f01f259d1a26fe3add2f5ac0d7
Frida Mwende <wawirafrida20@gmail.com>
2025-08-24 17:13:16 +0300

    Added Staff model

--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,11 @@
+
 from django.db import models
 
-# Create your models here.
+# This model is for staff management and is separate from CustomUser
+class Staff(models.Model):
+	username = models.CharField(max_length=150, unique=True)
+	email = models.EmailField(unique=True)
+	password = models.CharField(max_length=128)
+
+	def __str__(self):
+		return self.username
