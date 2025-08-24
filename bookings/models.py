8f6160150623c7df6e161b0ac27add533a579611
Frida Mwende <wawirafrida20@gmail.com>
2025-08-24 17:17:14 +0300

    Added Booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,26 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from accounts.models import CustomUser
+from staff.models import Staff
+from services.models import Service
+
+class Booking(models.Model):
+	STATUS_PENDING = 'pending'
+	STATUS_CONFIRMED = 'confirmed'
+	STATUS_COMPLETED = 'completed'
+	STATUS_CANCELED = 'canceled'
+	STATUS_CHOICES = [
+		(STATUS_PENDING, 'Pending'),
+		(STATUS_CONFIRMED, 'Confirmed'),
+		(STATUS_COMPLETED, 'Completed'),
+		(STATUS_CANCELED, 'Canceled'),
+	]
+
+	client = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+	staff = models.ForeignKey(Staff, on_delete=models.SET_NULL, null=True, blank=True)
+	service = models.ForeignKey(Service, on_delete=models.CASCADE)
+	appointment_time = models.DateTimeField()
+	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=STATUS_PENDING)
+
+	def __str__(self):
+		return f"Booking for {self.client.username} - {self.service.name} at {self.appointment_time}"
