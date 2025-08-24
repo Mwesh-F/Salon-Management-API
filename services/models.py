e991081b94751edfcce3ec4d40e222ae0a8f0924
Frida Mwende <wawirafrida20@gmail.com>
2025-08-24 17:08:11 +0300

    Added Service model with due_date and priority_level

--- services/models.py
+++ services/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+	LOW = 'Low'
+	MEDIUM = 'Medium'
+	HIGH = 'High'
+	PRIORITY_CHOICES = [
+		(LOW, 'Low'),
+		(MEDIUM, 'Medium'),
+		(HIGH, 'High'),
+	]
+
+	name = models.CharField(max_length=255)
+	description = models.TextField()
+	due_date = models.DateTimeField()
+	priority_level = models.CharField(max_length=6, choices=PRIORITY_CHOICES)
+
+	def __str__(self):
+		return self.name
