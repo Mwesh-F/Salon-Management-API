392f5e5a01e3b13d4ed1973227230d16caa41b10
Frida Mwende <wawirafrida20@gmail.com>
2025-08-24 16:54:50 +0300

    Added CustomUser model and configured AUTH_USER_MODEL

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+	username = models.CharField(max_length=150, unique=True)
+	email = models.EmailField(unique=True)
+	password = models.CharField(max_length=128)
+
+	REQUIRED_FIELDS = ['email']
+	USERNAME_FIELD = 'username'
+
+	def __str__(self):
+		return self.username
--- salon_management_api/settings.py
+++ salon_management_api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'services',
+    'staff',
+    'bookings',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+AUTH_USER_MODEL = 'accounts.CustomUser'
